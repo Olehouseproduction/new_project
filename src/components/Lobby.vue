8573ae6d6518f1a0aa9fd738bd01a8b70c45c3bf
Olesya <pachevaol@gmail.com>
2023-06-30 16:50:18 +0300

    made button

--- src/components/Lobby.vue
+++ src/components/Lobby.vue
@@ -7,18 +7,63 @@ const count = ref(0);
 <template>
   <div class="main">
     <!-- кнопки перехода по нашим заданиям-->
-    <router-link class="button-rout" to="/task_1">Задание №1</router-link>
-    <router-link class="button-rout" to="/task_2">Задание №2</router-link>
-    <router-link class="button-rout" to="/task_3">Задание №3</router-link>
-    <router-link class="button-rout" to="/task_4">Задание №4</router-link>
-    <router-link class="button-rout" to="/task_5">Задание №5</router-link>
-    <!-- //кнопки перехода по нашим заданиям-->
+    <router-link class="button-rout" to="/task_1">Задание 1</router-link>
+    <router-link class="button-rout" to="/task_2">Задание 2</router-link>
+    <router-link class="button-rout" to="/task_3">Задание 3</router-link>
+    <router-link class="button-rout" to="/task_4">Задание 4</router-link>
+    <router-link class="button-rout" to="/task_5">Задание 5</router-link>
   </div>
 </template>
 
 <style lang="scss" scoped>
+body {
+  background: #1b2631;
+  margin: 0 auto;
+  box-sizing: border-box;
+}
+//Стили body не подгружаются
 .main {
+  background: #1b2631;
+  width: 100vw;
+  height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.button-rout {
+  text-decoration: none;
+  display: inline-block;
+  margin: 10px 20px;
+  padding: 20px 60px;
+  position: relative;
+  border: 2px solid #f1c40f;
+  color: #f1c40f;
+  font-family: "Montserrat", sans-serif;
+  transition: 0.4s;
+}
+
+.button-rout:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   width: 100%;
   height: 100%;
+  margin: auto;
+  border: 2px solid rgba(0, 0, 0, 0);
+  transition: 0.4s;
 }
+.button-rout:hover:after {
+  border-color: #f1c40f;
+  width: calc(100% - 10px);
+  height: calc(100% + 10px);
+}
+
+//   //Вносить изменения сюда
+// }
 </style>
--- src/components/Task_1.vue
+++ src/components/Task_1.vue
@@ -10,6 +10,21 @@ const count = ref(0);
     <!-- Кнопка назад! -->
     <router-link class="button-rout" to="/">Назад</router-link>
     <!-- //Кнопка назад! -->
+    <section class="blocks">
+      <div class="wrapper">
+        <div>
+          <div class="header">header</div>
+          <div class="menu">menu</div>
+        </div>
+        <div class="hero">hero</div>
+        <div class="main1">main</div>
+        <div>
+          <div class="image">image</div>
+          <div class="extra">extra</div>
+        </div>
+        <div class="banner">banner</div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -18,11 +33,61 @@ const count = ref(0);
 // то их надо в начале добавить в этот тимплейт с помощью import
 @import "../styles/mixin.scss";
 
-.main {
-  position: relative;
-  width: 100vw;
-  height: 100vh;
+body {
+  background: #1b2631;
+  margin: 0 auto;
+  box-sizing: border-box;
 }
+
+.main {
+  display: grid;
+  width: 50vw;
+  height: 50vh;
+  margin: 0 auto;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+h1 {
+  color: white;
+  text-align: center;
+}
+
+.blocks {
+  text-transform: uppercase;
+}
+
+.header {
+  background-color: #17112b;
+  color: white;
+}
+
+.menu {
+  background-color: #ee3c52;
+  color: white;
+}
+
+.hero {
+  background-color: #4c86c6;
+  color: white;
+}
+
+.main1 {
+  background-color: #24344b;
+  color: white;
+}
+
+.image {
+  background-color: #c1e4e6;
+}
+
+.extra {
+  background-color: #4dbd97;
+}
+
+.banner {
+  background-color: #fed269;
+}
+
 .button-rout {
   @include position(absolute, $top: 5vh, $right: 5vw);
 }
