566077d665a3056c1ff22d8e91844a13f096fa2c
Denzel <lebedkin112@gmail.com>
2023-07-04 14:18:26 +0300

    add v-for components

---
+++ src/components/ui-componets/button_link.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+    url: String,
+    name: String
+})
+const count = ref(0);
+</script>
+
+<template>
+    <router-link class="button-rout" :to="props.url">{{ props.name }}</router-link>
+</template>
+
+<style lang="scss" scoped>
+@import "../../styles/mixin.scss";
+    .button-rout{
+        padding: 24px;
+        background-color: darkgreen;
+        color: aliceblue;
+        text-decoration: none;
+        border-radius: 12px;
+        transform: scale(1);
+        transition: all 350ms ease-in;
+        &:hover{
+            transform: scale(1.2);
+            background-color: darkcyan;
+        }
+    }
+</style>
---
+++ src/components/ui-componets/button_task.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+    name: String
+})
+const count = ref(0);
+</script>
+
+<template>
+    <div class="button-task">   
+        <span>
+            {{ props.name }}
+        </span>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../../styles/mixin.scss";
+    .button-task{
+        padding: 24px;
+        max-width: 120px;
+        min-height: 100px;
+        background-color: darkgreen;
+        border-radius: 12px;
+        transform: scale(1);
+        transition: all 350ms ease-in;
+        color: aliceblue;
+        @include flex (center, center);
+        span{
+            text-align: center;
+        }
+        &:hover{
+            animation: pulse 1.7s infinite;
+        }
+    }
+    @keyframes pulse {
+        0%   {transform: scale(1); background-color: darkgreen;}
+        50%  {transform: scale(1.2); background-color: darkcyan;}
+        100% {transform: scale(1); background-color: darkgreen;}
+    }
+</style>
